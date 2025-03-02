ae86596ceb6cbd2a475d4ed9d6a58ce378ae19b1
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2025-03-02 16:28:29 -0300

    alterando

--- rede_social/ap1_2.py
+++ rede_social/ap1_2.py
@@ -1,22 +1,72 @@
 from ap1_2_funcoes import *
+
+postagens = []
+comentarios = []
+
+nome = 'Gabriel'
+sobrenome = 'Vasconcelos'
+email = 'eugabrieldealbuquerque@gmail.com'
+celular = '11 12345-6789'
+data_nascimento = '25/02/2005'
+sexo = 'M'
+
+perfil2 = Perfil(nome, sobrenome, email, celular, data_nascimento, sexo)
+
+titulo = 'Il fait trop chaud'
+texto = 'Está muito quente'
+postagens.append(criar_postagem(titulo, texto))
+
+nome = 'Naiara'
+sobrenome = 'Alves'
+email = 'alvesnaiara@hotmail.com'
+celular = '11 12345-6789'
+data_nascimento = '17/04/1997'
+sexo = 'F'
+
+perfil3 = Perfil(nome, sobrenome, email, celular, data_nascimento, sexo)
+
+comentarios.append(comentar_postagem('Realmente! Está muito quente.'))
+
 print('_-' * 15, 'REDE SOCIAL ALB', '-_' * 15)
 
-print('\nSeja bem-vindo à Rede Social ALB. A seguir preencha os campos para criar o seu perfil: ')
+print('\nSeja bem-vindo à Rede Social ALB!')
+print('\nA seguir preencha os campos para criar o seu perfil: ')
 
+# Solicitando dados e atribuindo a variáveis
 nome = input('\nDigite o seu nome: ')
 sobrenome = input('Digite o seu sobrenome: ')
 email = input('Digite o seu email: ')
 celular = input('Digite o seu número de celular: ')
 data_nascimento = input('Digite a sua data de nascimento: ')
-sexo = input('Digite o seu sexo: ').upper
+sexo = input('Digite o seu sexo: ')
 
-perfil1 = RedeSocial(nome, sobrenome, email, celular, data_nascimento, sexo)
+perfil_usuario  = Perfil(nome, sobrenome, email, celular, data_nascimento, sexo)
+# Criando variável a qual chama a classe Perfil e passa como parâmetro todos os dados inseridos.
 
-print('\nSeu perfil foi criado com os seguintes dados: ')
-print(f'\nNome: {nome}')
-print(f'Sobrenome: {sobrenome}')
-print(f'Email: {email}')
-print(f'Celular: {celular}')
-print(f'Data de Nascimento: {data_nascimento}')
-print(f'Sexo: {sexo}')
+# Chamando a função exibir dados para mostrar ao usuário os dados passados.
+print('\nPerfil criado com sucesso!')
+perfil_usuario.exibir_dados()
 
+criar_post = input('\nDeseja criar uma postagem? (S/N) ').upper()
+while criar_post != 'S' and criar_post != 'N':
+    criar_post = input('\nErro: digite apenas S ou N: ').upper()
+while criar_post == 'S':
+    titulo = input('\nQual será o título da postagem? ')
+    texto = input('\nDigite a postagem: ')
+    postagens.append(criar_postagem(titulo, texto))
+    postagens.exibir_postagem()
+    criar_post = input('\nDeseja criar mais uma postagem? (S/N) ').upper()
+
+print('\nPostagens do dia: ')
+print(f'\n{perfil2.nome}')
+postagens.exibir_postagem()
+
+print('\nComentários: ')
+print(f'{perfil3.nome}')
+comentarios.exibir_comentario()
+
+comentario = input('Deseja comentar algum post? (S/N) ').upper()
+while comentario != 'S' and comentario != 'N':
+    comentario = input('Erro: digite S ou N: ').upper()
+while comentario == 'S':
+    comentarios.append(input('Digite seu comentário: '))
--- rede_social/ap1_2_funcoes.py
+++ rede_social/ap1_2_funcoes.py
@@ -1,22 +1,35 @@
 import datetime
 
-class RedeSocial: # classe chamada rede social a qual o def init é a criação de perfil
+class Perfil: # classe de criação de perfil 
     def __init__(self, nome:str, sobrenome:str, email:str, celular:str, data_nasc:str, sexo: str):
         self.nome = nome
         self.sobrenome = sobrenome
         self.email = email
         self.celular = celular
         self.data_nascimento = data_nasc
-        self.sexo = sexo
+        self.sexo = sexo.upper()    
 
-    # metódo que permite adicionar amigos pelo nome completo
-    def adicionar_amigos(self, nome_completo:str):
-        self.adicionar_amigos = nome_completo
+    def exibir_dados(self):
+        print(f'\nNome: {self.nome}\nSobrenome: {self.sobrenome}\nEmail: {self.email}\nCelular: {self.celular}\nData de Nascimento: {self.data_nascimento}\nSexo: {self.sexo}')
+
 
     # metódo que permite criar postagem com um texto
-    def criar_postagem(self, texto:str):
-        self.criar_postagem = texto
+class criar_postagem:
+    def __init__(self, titulo:str, texto:str):
+        self.titulo = titulo
+        self.texto = texto
 
+    def exibir_postagem(self):
+        print(f'Título: {self.titulo}\nPostagem: {self.texto}.')
     # metódo que permite comentár na postagem por meio de comentário 
-    def comentar_postagem(self, comentario:str):
-        self.comentar_postagem = comentario
+
+class comentar_postagem:
+        def __init__(self, comentario:str):
+            self.comentar_postagem = comentario
+        
+        def exibir_comentario(self):
+            print(f'{self.comentar_postagem}')
+            
+def adicionar_amigos(self, nome_completo:str):
+    self.adicionar_amigos = nome_completo
+
