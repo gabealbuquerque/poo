6379670ae86e37b912756f4d25a62e6cd9504a21
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2025-03-07 10:00:01 -0300

    att

--- rede_social/ap1_2_funcoes.py
+++ rede_social/ap1_2_funcoes.py
@@ -1,35 +1,25 @@
 import datetime
 
-class Perfil: # classe de criação de perfil 
-    def __init__(self, nome:str, sobrenome:str, email:str, celular:str, data_nasc:str, sexo: str):
+class Perfil:
+    def __init__(self, nome:str, sobrenome:str, data_nascimento:str, email:str, telefone:str, sexo:str):
         self.nome = nome
         self.sobrenome = sobrenome
+        self.data_nascimento = data_nascimento
         self.email = email
-        self.celular = celular
-        self.data_nascimento = data_nasc
-        self.sexo = sexo.upper()    
-
-    def exibir_dados(self):
-        print(f'\nNome: {self.nome}\nSobrenome: {self.sobrenome}\nEmail: {self.email}\nCelular: {self.celular}\nData de Nascimento: {self.data_nascimento}\nSexo: {self.sexo}')
-
-
-    # metódo que permite criar postagem com um texto
-class criar_postagem:
-    def __init__(self, titulo:str, texto:str):
-        self.titulo = titulo
-        self.texto = texto
-
-    def exibir_postagem(self):
-        print(f'Título: {self.titulo}\nPostagem: {self.texto}.')
-    # metódo que permite comentár na postagem por meio de comentário 
-
-class comentar_postagem:
-        def __init__(self, comentario:str):
-            self.comentar_postagem = comentario
+        self.celular = telefone
+        self.sexo = sexo
         
-        def exibir_comentario(self):
-            print(f'{self.comentar_postagem}')
-            
-def adicionar_amigos(self, nome_completo:str):
-    self.adicionar_amigos = nome_completo
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome}'    
 
+    def exibePerfil(self):
+        print(f'\nInformações do seu perfil:\n \nNome: {self.nome}\nSobrenome: {self.sobrenome}\nData de Nascimento: {self.data_nascimento}\nEmail: {self.email}\nNúmero de telefone: {self.celular}\nSexo: {self.sexo}')
+
+
+class Postagem:
+    def __init__(self, perfil, texto):
+        self.perfil = perfil
+        self.texto = texto
+    
+    def exibePostagem(self):
+         return f'{self.perfil.nome_completo()} \n{self.texto()}'
